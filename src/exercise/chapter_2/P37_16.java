6aec8f2366d3b809fda769a8e97800676326a57e
fys <849485789@qq.com>
2018-09-27 13:30:19 +0800

    习题进度更新

--- src/exercise/chapter_2/P37_16.java
+++ src/exercise/chapter_2/P37_16.java
@@ -6,5 +6,52 @@ package exercise.chapter_2;
  *  一个HTML文件，最后，通过Web浏览器查看结果。
  */
 public class P37_16 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Tree tree = new Tree(i);
+            tree.info();
+            tree.info("Overloaded method");
+        }
+        //Overloaded constructor:
+        new Tree();
+    }
+}
 
+class Tree{
+    /**
+     * 树的高度
+     */
+    int height;
+
+    /**
+     * 普通构造方法
+     */
+    Tree() {
+        System.out.println("Planting a seedling");
+        height = 0;
+    }
+
+    /**
+     * 可以初始化默认高度的构造方法
+     * @param initialHeight 树的默认高度
+     */
+    Tree(int initialHeight) {
+        height = initialHeight;
+        System.out.println("Creating new Tree that is " + height + "feet tall");
+    }
+
+    /**
+     *输出树的信息
+     */
+    void info() {
+        System.out.println("Tree is " + height + " feet tall");
+    }
+
+    /**
+     * info()方法的重载方法，多接收一个参数。输出树的信息
+     * @param s
+     */
+    void info(String s) {
+        System.out.println(s + ": Tree is " + height + " feet tall");
+    }
 }
